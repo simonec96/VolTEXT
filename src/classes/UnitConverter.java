147971e4915602c8948c5e2d5b9025a1a4b7f714
StefanoScarpellini <s.scarpellini5@studenti.unibg.it>
2021-02-26 16:09:51 +0100

    1.0.6
    - aggiunta metodi supplementari classe UnitConverter

--- src/classes/UnitConverter.java
+++ src/classes/UnitConverter.java
@@ -4,8 +4,14 @@ public class UnitConverter {
 	public static Float convmmPoint(Float mm) {
 		return (float) ((mm/25.4)*72);
 	}
+	public static Float convmmPoint(Integer mm) {
+		return convmmPoint((float)mm);
+	}
 	public static Float convPointmm(Integer point) {
 		return (float) ((point/72)*25.4);
+	}
+	public static Float convPointmm(Float point) {
+		return convPointmm(point.intValue());
 	}
 	public static Float convPixelmm(Integer px) {
 		return px*37.795275591f;
@@ -18,5 +24,17 @@ public class UnitConverter {
 	}
 	public static Float convPointPixel(Integer point) {
 		return convmmPixel(convPointmm(point));
+	}
+	public static Float convPixelmm(Float px) {
+		return convPixelmm(px.intValue());
+	}
+	public static Float convmmPixel(Integer mm) {
+		return convmmPixel((float)mm);
+	}
+	public static Float convPixelPoint(Float px) {
+		return convPixelPoint(px.intValue());
+	}
+	public static Float convPointPixel(Float point) {
+		return convPointPixel(point.intValue());
 	}
 }
